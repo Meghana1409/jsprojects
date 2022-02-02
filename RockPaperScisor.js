48985afbe872ad80b0cf79763bf5d0fb29df0127
Meghana Ravindra Bhagat <meghanabhagat96872@gmail.com>
2022-02-02 20:02:23 +0530

    Create RockPaperScisor.js

---
+++ RockPaperScisor.js
@@ -0,0 +1,18 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+    
+console.log("Welcome to Programiz!");
+let answers = ["rock", "paper", "scissor"];
+let userchoice=prompt("Choose rock / paper or scissor").toLowerCase();
+let compschoice = answers[Math.floor(Math.random()*answers.length)]
+
+if(!userchoice || answers.indexOf(userchoice)==-1){
+    console.log("Please enter valid choice!!!");
+}
+else if(userchoice === compschoice) {console.log( "Tie!");}
+
+else if(userchoice === "rock" && compschoice === "paper") {console.log ("You lose, computer picked " +  compschoice);}
+ else if(userchoice === "paper" && compschoice === "scissor") {console.log ("You lose, computer picked " +  compschoice);}
+ else if(userchoice === "scissor" && compschoice === "rock") {console.log ("You lose, computer picked " +  compschoice);}
+else{
+  console.log ("You win! Computer picked " +  compschoice);}
