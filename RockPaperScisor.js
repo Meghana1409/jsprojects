e0b21909205f49d187d42672e1fcca31d1e3839b
Meghana Ravindra Bhagat <meghanabhagat96872@gmail.com>
2022-02-02 20:03:50 +0530

    Update RockPaperScisor.js

--- RockPaperScisor.js
+++ RockPaperScisor.js
@@ -6,10 +6,18 @@ let compschoice = answers[Math.floor(Math.random()*answers.length)]
 if(!userchoice || answers.indexOf(userchoice)==-1){
     console.log("Please enter valid choice!!!");
 }
-else if(userchoice === compschoice) {console.log( "Tie!");}
-
-else if(userchoice === "rock" && compschoice === "paper") {console.log ("You lose, computer picked " +  compschoice);}
- else if(userchoice === "paper" && compschoice === "scissor") {console.log ("You lose, computer picked " +  compschoice);}
- else if(userchoice === "scissor" && compschoice === "rock") {console.log ("You lose, computer picked " +  compschoice);}
+else if(userchoice === compschoice) {
+    console.log( "Tie!");
+}
+else if(userchoice === "rock" && compschoice === "paper") {
+    console.log ("You lose, computer picked " +  compschoice);
+}
+else if(userchoice === "paper" && compschoice === "scissor") {
+    console.log ("You lose, computer picked " +  compschoice);
+}
+else if(userchoice === "scissor" && compschoice === "rock") {
+    console.log ("You lose, computer picked " +  compschoice);
+}
 else{
-  console.log ("You win! Computer picked " +  compschoice);}
+  console.log ("You win! Computer picked " +  compschoice);
+}
